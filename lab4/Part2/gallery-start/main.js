f3aceadb6def16fb2845e438df800cfcba5f13de
NobleRhed <pod_routine_06@icloud.com>
2024-04-04 15:51:56 -0400

    Part 2 work
    Preemptive adding of mandatory comment lines

--- lab4/Part2/gallery-start/main.js
+++ lab4/Part2/gallery-start/main.js
@@ -5,6 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imagesStored = ['./images/pic1.jpg', './images/pic2.jpg', './images/pic3.jpg', './images/pic4.jpg', './images/pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 
@@ -16,11 +17,20 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
-btn.addEventListener('click', )
+btn.addEventListener('click', lightdark)
+const currentClass = btn.getAttribute('class')
 
-if (btn.getAttribute("class","dark")) {
-    btn.setAttribute("class", "light");
-    btn.textContent = "Lighten";
-    overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+function lightdark() {
+    if (currentClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+    }
+
 }
+
 
